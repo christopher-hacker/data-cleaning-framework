1cef08f3c953e0f2db226860f8012dad02c49b1b
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-11-09 17:00:20 +0000

    docstrings

--- data_cleaning_framework/cli.py
+++ data_cleaning_framework/cli.py
@@ -1,14 +1,17 @@
+"""Command line interface for data cleaning framework"""
 import click
 
 
 @click.group()
 def cli():
-    pass
+    """Command line interface for data cleaning framework"""
+    pass  # pylint: disable=unnecessary-pass
 
 
 @click.command()
 @click.argument("config_file")
 def run_task(config_file):
+    """Run a data cleaning task with a given config file"""
     click.echo(f"run_task with config_file: {config_file}")
 
 
