fb7df51dca297fb9228de238d832eaf367f09bf5
Chris Hacker <49451910+christopher-hacker@users.noreply.github.com>
2023-11-09 17:07:22 +0000

    copy over options from project

--- data_cleaning_framework/cli.py
+++ data_cleaning_framework/cli.py
@@ -10,9 +10,37 @@ def cli():
 
 @click.command()
 @click.argument("config_file")
-def run_task(config_file):
+@click.option(
+    "--test-run",
+    is_flag=True,
+    help="Run in test mode, cleaning only the last file in the config file",
+)
+@click.option(
+    "--threads",
+    type=int,
+    default=1,
+    help="Number of threads to use for cleaning. Specifying more than 1 will "
+    "run multiple files in parallel. Defaults to 1.",
+)
+@click.option(
+    "--scenario",
+    type=str,
+    default="default",
+    help="The name of a scenario to run extra cleaners from. Must be a name specified in the "
+    "cleaner decorator of at least one cleaner function. All default cleaners will be run "
+    "regardless of this option. If not provided, only default cleaners will be run. "
+    "You can also provide the string 'default' to run with the same cleaners as the default.",
+)
+def run_task(
+    config_file,
+    test_run,
+    threads,
+    scenario,
+):
     """Run a data cleaning task with a given config file"""
-    click.echo(f"run_task with config_file: {config_file}")
+    click.echo(
+        f"run_task with config_file: {config_file}, test_run: {test_run}, threads: {threads}, scenario: {scenario}"
+    )
 
 
 cli.add_command(run_task)
